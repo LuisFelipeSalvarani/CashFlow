5c14a8c857498df3dd115acaf93f57f9f380f7db
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-03-19 15:47:20 -0300

    Criação de um Database Context

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashflow;Uid=docker;Pwd=docker";
+        var version = new Version(8, 4, 4);
+        var serverVersion = new MySqlServerVersion(version);
+
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
