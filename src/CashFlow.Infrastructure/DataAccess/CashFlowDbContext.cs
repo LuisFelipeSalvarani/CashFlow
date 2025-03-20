5907af0c17df105fe9611a39d7ece27379f46956
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-03-20 11:27:56 -0300

    AppSettings para a Connection string

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -5,14 +5,7 @@ namespace CashFlow.Infrastructure.DataAccess;
 
 internal class CashFlowDbContext : DbContext
 {
+    public CashFlowDbContext(DbContextOptions options) : base(options) { }
+
     public DbSet<Expense> Expenses { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var connectionString = "Server=localhost;Database=cashflow;Uid=docker;Pwd=docker";
-        var version = new Version(8, 4, 4);
-        var serverVersion = new MySqlServerVersion(version);
-
-        optionsBuilder.UseMySql(connectionString, serverVersion);
-    }
 }
--- src/CashFlow.Infrastructure/DepndenceInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DepndenceInjectionExtension.cs
@@ -1,15 +1,17 @@
 ﻿using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CashFlow.Infrastructure;
 
 public static class DepndenceInjectionExtension
 {
-    public static void AddInfrastructure(this IServiceCollection services)
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        AddDbContext(services);
+        AddDbContext(services, configuration);
         AddRepositories(services);
     }
 
@@ -18,8 +20,13 @@ public static class DepndenceInjectionExtension
         services.AddScoped<IExpensesRepository, ExpensesRepository>();
     }
 
-    private static void AddDbContext(IServiceCollection services)
+    private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<CashFlowDbContext>();
+        var connectionString = configuration.GetConnectionString("Connection");
+
+        var version = new Version(8, 4, 4);
+        var serverVersion = new MySqlServerVersion(version);
+
+        services.AddDbContext<CashFlowDbContext>(config => config.UseMySql(connectionString, serverVersion));
     }
 }
