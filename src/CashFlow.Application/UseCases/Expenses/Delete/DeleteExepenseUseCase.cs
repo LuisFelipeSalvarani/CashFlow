0d2b40504924260fd89f93d1d60549e642453b0c
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-03-24 13:29:23 -0300

    Remoção de despesa

---
+++ src/CashFlow.Application/UseCases/Expenses/Delete/DeleteExepenseUseCase.cs
@@ -0,0 +1,30 @@
+﻿using CashFlow.Domain.Repositories;
+using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExeceptionsBase;
+
+namespace CashFlow.Application.UseCases.Expenses.Delete;
+
+public class DeleteExepenseUseCase : IDeleteExpenseUseCase
+{
+    private readonly IExpensesWriteOnlyRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteExepenseUseCase(IExpensesWriteOnlyRepository repository, IUnitOfWork unitOfWork)
+    {
+        _repository = repository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Execute(long id)
+    {
+        var result = await _repository.Delete(id);
+
+        if(result == false)
+        {
+            throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+        }
+
+        await _unitOfWork.Commit();
+    }
+}
--- src/CashFlow.Domain/Repositories/Expenses/IExpensesWriteOnlyRepository.cs
+++ src/CashFlow.Domain/Repositories/Expenses/IExpensesWriteOnlyRepository.cs
@@ -4,4 +4,10 @@ namespace CashFlow.Domain.Repositories.Expenses;
 public interface IExpensesWriteOnlyRepository
 {
     Task Add(Expense expense);
+    /// <summary>
+    /// This function returns TRUE if the deletation was seccessful otherwise returns FALSE
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    Task<bool> Delete(long id);
 }
